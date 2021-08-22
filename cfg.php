f573d476272d1f4ab17bef293a219d9aa79eb869
kavin31287 <74664184+kavin31287@users.noreply.github.com>
2021-08-22 17:43:13 +0530

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,5 @@
+<?php
+
+$uag = "Mozilla/5.0 (Linux; Android 11; RMX2117) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36";
+
+$cookie = "session=13af69cd-e069-44ce-bcbd-1e80e241d743.7ApI7Vwd_pi6b1aFjuGMh4etLgc;bitmedia_fid=eyJmaWQiOiJlM2YwNDA2YmI1ZWMzYzE0Y2NhMTRlYjdiYjg0YTI0YiIsImZpZG5vdWEiOiIwZDlkYTI5YTU2NWY5NWVlM2YzOTU3MzY0YzNiODdmZSJ9;_ga=GA1.2.749764310.1629633215;_gid=GA1.2.359813055.1629633215;_gat_gtag_UA_113361696_1=1;_data_cpm=340-1_347-1";
